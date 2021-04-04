97fa2fec56641089d222d90ddb603a2671e0ffa8
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2021-04-04 01:31:26 -0300

    feat: Added todos and database integration

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:professional_todo/models/todo.dart';
 import 'package:professional_todo/services/auth.dart';
+import 'package:professional_todo/services/database.dart';
+import 'package:professional_todo/widgets/todo_card.dart';
 
 class Home extends StatefulWidget {
   final FirebaseAuth auth;
@@ -18,17 +21,98 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextEditingController _todoController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Professional TODO"),
+        centerTitle: true,
         actions: [
           IconButton(
             icon: const Icon(Icons.exit_to_app),
             onPressed: () async {
               Auth(auth: widget.auth).signOut();
             },
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Add Todo: ",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.all(20),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextFormField(
+                    controller: _todoController,
+                  )),
+                  IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: () {
+                      if (_todoController.text != "") {
+                        setState(() {
+                          Database(firestore: widget.firestore).addTodo(
+                              uid: widget.auth.currentUser.uid,
+                              content: _todoController.text);
+                          _todoController.clear();
+                        });
+                      }
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Your TODOs",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+            child: StreamBuilder(
+              stream: Database(firestore: widget.firestore)
+                  .streamTodos(uid: widget.auth.currentUser.uid),
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<TodoModel>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.active) {
+                  if (snapshot.data == null) {
+                    return const Center(
+                      child: Text("You don't have any unfinished TODOs"),
+                    );
+                  }
+
+                  return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (_, index) {
+                      return TodoCard(
+                          firestore: widget.firestore,
+                          uid: widget.auth.currentUser.uid,
+                          todo: snapshot.data[index]);
+                    },
+                  );
+                } else {
+                  return const Center(
+                    child: Text("Loading..."),
+                  );
+                }
+              },
+            ),
           )
         ],
       ),
